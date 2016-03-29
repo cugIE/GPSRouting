f43893c8c11e9b8c875ef92840b1bf5bbb7b8962
lixin1994 <lixin_cug@foxmail.com>
2016-03-29 11:26:18 +0800

    更新question的增删改查

--- GPSRouting/src/com/bean/Question.java
+++ GPSRouting/src/com/bean/Question.java
@@ -1,6 +1,11 @@
 package com.bean;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.util.DBHelper;
 
 /**
  * Question class to assemble all components
@@ -13,8 +18,9 @@ public class Question {
 	private String possibleAsw;
 	private String normalAsw;
 	private String region;
-	private String regionId;
+	private int region_id;
 	private String gener;
+	private int gener_id;
 	/**
 	 * Constructor
 	 */
@@ -25,7 +31,8 @@ public class Question {
 		this.normalAsw = null;
 		this.region = null;
 		this.gener = null;
-		this.regionId = null;
+		this.region_id = 0;
+		this.gener_id = 0;
 	}
 	/**
 	 * Constructor with its variables
@@ -37,7 +44,7 @@ public class Question {
 	 * @param gener
 	 */
 	public Question(String id, String title, String possibleAsw,
-			String normalAsw, String region, String gener, String regionId) {
+			String normalAsw, String region, String gener, int region_id, int gener_id) {
 		super();
 		this.id = id;
 		this.title = title;
@@ -45,14 +52,22 @@ public class Question {
 		this.normalAsw = normalAsw;
 		this.region = region;
 		this.gener = gener;
-		this.regionId = regionId;
+		this.region_id = region_id;
+		this.gener_id = gener_id;
 	}
 
-	public String getRegionId() {
-		return regionId;
+	
+	public int getRegion_id() {
+		return region_id;
 	}
-	public void setRegionId(String regionId) {
-		this.regionId = regionId;
+	public void setRegion_id(int region_id) {
+		this.region_id = region_id;
+	}
+	public int getGener_id() {
+		return gener_id;
+	}
+	public void setGener_id(int gener_id) {
+		this.gener_id = gener_id;
 	}
 	public String getId() {
 		return id;
@@ -95,34 +110,83 @@ public class Question {
 	 * Add one Question
 	 * @param qs
 	 * @return
+	 * @throws SQLException 
 	 */
-	public static boolean addOneQuestion(Question qs){
-		return false;
+	public static int addOneQuestion(Question qs) throws SQLException{
+		int result = 0;
+		if (qs == null){
+			result = -1;
+		}
+		else{
+			String sql = "inset into question "
+					+ "(question_title, question_asws, question_r_asws, gener_id, åregion_id)"
+					+ "values('"+ qs.getTitle() + "','"
+					+ qs.getPossibleAsw() + "','" 
+					+ qs.getNormalAsw()  + "','" 
+					+ qs.getGener_id()  + "','" 
+					+ qs.getRegion_id()  + "')"; 
+			DBHelper dbh = new DBHelper();
+			result = dbh.updateDatabase(sql);
+			dbh.DBClose();
+		}
+		return result;
 	}
 	/**
 	 * Delete one Question
 	 * @param qID
 	 * @return
+	 * @throws SQLException 
 	 */
-	public static boolean deleteOneQuestion(String qID){
-		return false;
-	}
+	public static int deleteOneQuestion(String id) throws SQLException{
+		if (id == null){
+			return -1;
+		}
+		else{
+			String sql = "delete from question "
+					+ "where question_id=" + id;
+			DBHelper dbh = new DBHelper();
+			int result = dbh.updateDatabase(sql);
+			dbh.DBClose();
+			return result;
+		}	}
 	
 	/**
 	 * Get all questions
 	 * @return
 	 */
-	public static List<Question> getAllQuestion(){
-		return null;
-	}
+//	public static List<Question> getAllQuestion(){
+//
+//	}
 	
 	/**
 	 * Get all questions from one region;
 	 * @param rgif
 	 * @return
+	 * @throws SQLException 
 	 */
-	public static List<Question> getAllQuestion(String rgid){
-		return null;
+	public static List<Question> getAllQuestion(String rgid) throws SQLException{
+		List<Question> qts = new ArrayList<Question>();
+		String sql = "SELECT question_id, question_title, question_asws, question_r_asws, question.gener_id, people_name"
+				+ "from "
+				+ "question "
+				+ "inner join people "
+				+ "on question.gener_id = people.people_id"
+				+ "where question.region_id =" + rgid;
+		DBHelper dbh = new DBHelper();
+		ResultSet rs = dbh.getResultSet(sql);
+		while(rs.next()){
+			Question qt = new Question();
+			qt.setId(rs.getString(1));
+			qt.setTitle(rs.getString(2));
+			qt.setPossibleAsw(rs.getString(3));
+			qt.setNormalAsw(rs.getString(4));
+			qt.setGener_id(rs.getInt(5));
+			qt.setGener(rs.getString(6));
+			qt.setRegion_id(Integer.parseInt(rgid));
+			qts.add(qt);
+		}
+		dbh.DBClose(rs);
+		return qts;
 	}
 	
 	

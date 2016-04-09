2e9b97ed4b59e55c2eb3174a37460a363828671f
lixin1994 <lixin_cug@foxmail.com>
2016-04-09 22:25:59 +0800

    新增图片上传模块

--- GPSRouting/WebRoot/assets/js/pages/data-region-detail.js
+++ GPSRouting/WebRoot/assets/js/pages/data-region-detail.js
@@ -12,19 +12,19 @@ $(document).ready(function(){
 		$("#region-edit-range").val($("#region-range").text());
 		$("#region-edit-static").text($("#region-id").text());
 	});
-	$(".question-edit").on("click", function(){
-		var td=$(this).parent(); 
-		var tr=td.parent();
-		var id = tr.attr("value");
-		var title = tr.children('td').eq(0).text();
-		var possasws = tr.children('td').eq(1).text();
-		var normalasws = tr.children('td').eq(2).text();
-		$("#question-edit-id").val(id);
-		$("#question-edit-title").val(title);
-		$("#question-edit-possasws").val(possasws);
-		$("#question-edit-normalasws").val(normalasws);
-		
-	});
+	 $(document).on("click",".question-edit",function(){
+			var td=$(this).parent(); 
+			var tr=td.parent();
+			var id = tr.attr("value");
+			var title = tr.children('td').eq(0).text();
+			var possasws = tr.children('td').eq(1).text();
+			var normalasws = tr.children('td').eq(2).text();
+			$("#question-edit-id").val(id);
+			$("#question-edit-title").val(title);
+			$("#question-edit-possasws").val(possasws);
+			$("#question-edit-normalasws").val(normalasws);
+			
+		});
 	$('#changeregion').submit(function() {
 	    // submit the form
 	    $(this).ajaxSubmit(function(message){
@@ -72,8 +72,8 @@ $(document).ready(function(){
 		    	if (message != 'error'){
 		    		alert("添加成功成功,编号为"+message);
 		    		var actions="<a class = \"question-edit pull-right bk-margin-left-10\" data-toggle=\"modal\" data-target=\"#myModal\" href = \"#\" value=\""+ message +"\"><i class=\"fa fa-pencil fa-2x\"></i></a>"
-//<a class = \"pull-right question-delete\" value=\""+ message+"\" href = \"#\"><i class=\"fa fa-trash-o fa-2x\"></i></a>
-		    		var str = "<tr id="+ message +">"+"<td>"+$("#question-title").val()+"</td>"+"<td>"+$("#question-possasws").val()+"</td>"+"<td>"+$("#question-normalasws").val()+"</td>";
+		    		var bars = "<a class = \"pull-right question-delete\" value=\""+ message+"\" href = \"#\"><i class=\"fa fa-trash-o fa-2x\"></i></a>"
+		    		var str = "<tr id="+ message +">"+"<td>"+$("#question-title").val()+"</td>"+"<td>"+$("#question-possasws").val()+"</td>"+"<td>"+$("#question-normalasws").val()+ actions + bars + "</td>";
 		    		
 		    		$("#questions-table").append(str);
 		    		$('#addquestion').resetForm();
@@ -85,7 +85,35 @@ $(document).ready(function(){
 		    });
 		    return false;
 		});
-	 $("td").delegate(".question-delete","click",function(){
+//	 $("td").delegate(".question-delete","click",function(){
+//		var question_id = $(this).attr("value");
+//		var td=$(this).parent(); 
+//		var tr=td.parent(); 
+//		if(confirm("确定要删除编号为"+question_id+"的问题?")){
+//			$.post("DeleteSingleQuestionServlet",
+//			  {
+//			    question_id: question_id
+//			  },
+//			  function(data,status){
+//				  if(status == "success"){
+//					  if (data=="1"){
+//						  alert("删除成功");
+//						  tr.remove();
+//					  }
+//					  else{
+//						  alert("删除失败");
+//					  }
+//				  }
+//				  else{
+//					  alert("链接超时");
+//				  }
+//				  
+//			  });
+//			}else{
+//				
+//			}
+//		});
+	 $(document).on("click",".question-delete",function(){
 		var question_id = $(this).attr("value");
 		var td=$(this).parent(); 
 		var tr=td.parent(); 

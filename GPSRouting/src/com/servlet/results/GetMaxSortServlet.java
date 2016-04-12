31275f47969f94e12acc657214d61bdf6e189d83
lixin1994 <lixin_cug@foxmail.com>
2016-04-12 17:00:18 +0800

    补充sort的servlet

--- GPSRouting/src/com/servlet/results/GetMaxSortServlet.java
+++ GPSRouting/src/com/servlet/results/GetMaxSortServlet.java
@@ -1,11 +1,16 @@
 package com.servlet.results;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.bean.PtrConnection;
+import com.util.OutputHelper;
 
 /**
  * Servlet implementation class GetMaxSortServlet
@@ -34,6 +39,27 @@ public class GetMaxSortServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String period_id = request.getParameter("period_id");
+		if(period_id == null){
+			OutputHelper.StringOutPut("error_period_id", response);
+		}
+		else{
+			try {
+				int max = PtrConnection.getMaxSort(period_id);
+				if (max == -1){
+					System.out.println(max);
+					OutputHelper.StringOutPut("error", response);
+					return;
+				}
+				else {
+					OutputHelper.StringOutPut(""+max, response);
+					return;
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
--- GPSRouting/src/com/servlet/results/SwitchSortServlet.java
+++ GPSRouting/src/com/servlet/results/SwitchSortServlet.java
@@ -1,11 +1,16 @@
 package com.servlet.results;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.bean.PtrConnection;
+import com.util.OutputHelper;
 
 /**
  * Servlet implementation class SwitchSortServlet
@@ -36,6 +41,17 @@ public class SwitchSortServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		String ptr_id_bef = request.getParameter("ptr_id_bef");
 		String ptr_id_aft = request.getParameter("ptr_id_aft");
+		try {
+			if(PtrConnection.switchSort(ptr_id_bef, ptr_id_aft)){
+				OutputHelper.StringOutPut("success", response);
+			}
+			else{
+				OutputHelper.StringOutPut("fail", response);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 	}

28953890731a62b60832ca275a33001e1ff0e3c0
lixin1994 <lixin_cug@foxmail.com>
2016-04-16 20:49:03 +0800

    添加新的bean postion 和 route

---
+++ GPSRouting/src/com/bean/Position.java
@@ -0,0 +1,154 @@
+package com.bean;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import com.util.DBHelper;
+
+public class Position {
+	private int gener_id;
+	//private String gener;
+	private float longitude;
+	private float latitude;
+	private int iscurrent;
+	private String time;
+	public Position(int gener_id, float longitude, float latitude, int iscurrent,
+			String time) {
+		super();
+		this.gener_id = gener_id;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.iscurrent = iscurrent;
+		this.time = time;
+	}
+	public int getGener_id() {
+		return gener_id;
+	}
+	public void setGener_id(int gener_id) {
+		this.gener_id = gener_id;
+	}
+	public float getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+	public float getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+	public int getIscurrent() {
+		return iscurrent;
+	}
+	public void setIscurrent(int iscurrent) {
+		this.iscurrent = iscurrent;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+	
+	public static JSONArray GetAllPosition(String gener_id, Timestamp start, Timestamp end) throws SQLException{
+		JSONArray positions= new JSONArray();
+		String sql = "select people_name, pos_longitude, pos_latitude, pos_time, pos_iscurrent "
+				+ "from position inner join people "
+				+ "on people_id = position.gener_id "
+				+ "where position.gener_id = " + gener_id +" and "
+				+ "pos_time < " + end + " and pos_time > " + start + " order by pos_time";
+		DBHelper dbh = new DBHelper();
+		ResultSet rs = dbh.getResultSet(sql);
+		while(rs.next()){
+			JSONObject pos = new JSONObject();
+			pos.put("gener", rs.getString(1));
+			pos.put("longitude", rs.getString(2));
+			pos.put("latitude", rs.getString(3));
+			pos.put("time", rs.getString(4));
+			pos.put("iscurrent", rs.getString(5));
+			positions.add(pos);
+		}
+		dbh.DBClose(rs);
+		return positions;
+	}
+	public static JSONArray GetAllPosition() throws SQLException{
+		JSONArray positions= new JSONArray();
+		//Map<String, String> position = new HashMap<String, String>();
+		String sql = "select people_name, pos_longitude, pos_latitude, pos_time "
+				+ "from position inner join people "
+				+ "on people_id = position.gener_id "
+				+ "where position.iscurrent = 1";
+		DBHelper dbh = new DBHelper();
+		ResultSet rs = dbh.getResultSet(sql);
+		while(rs.next()){
+			JSONObject pos = new JSONObject();
+			pos.put("gener", rs.getString(1));
+			pos.put("longitude", rs.getString(2));
+			pos.put("latitude", rs.getString(3));
+			pos.put("time", rs.getString(4));
+			positions.add(pos);
+		}
+		dbh.DBClose(rs);
+		return positions;
+	}
+	public static JSONArray GetAllPosition(String gener_id) throws SQLException{
+		JSONArray positions= new JSONArray();
+		//Map<String, String> position = new HashMap<String, String>();
+		String sql = "select people_name, pos_longitude, pos_latitude, pos_time "
+				+ "from position inner join people "
+				+ "on people_id = position.gener_id "
+				+ "where position.iscurrent = 1 and "
+				+ "position.gener_id = " + gener_id;
+		DBHelper dbh = new DBHelper();
+		ResultSet rs = dbh.getResultSet(sql);
+		while(rs.next()){
+			JSONObject pos = new JSONObject();
+			pos.put("gener", rs.getString(1));
+			pos.put("longitude", rs.getString(2));
+			pos.put("latitude", rs.getString(3));
+			pos.put("time", rs.getString(4));
+			positions.add(pos);
+		}
+		dbh.DBClose(rs);
+		return positions;
+	}
+	public static int addOnePosition(Position pst) throws SQLException{
+		int result = 0;
+		if (pst == null){
+			result = -1;
+		}
+		else{
+			String sql = "insert into position"
+					+ "(gener_id, pos_longitude, pos_latitude)"
+					+ " values('"+pst.getGener_id()+ "','" 
+					+ pst.getLongitude() + "','" 
+					+ pst.getLatitude() +  "')"; 
+			DBHelper dbh = new DBHelper();
+			result = dbh.updateDatabase(sql);
+			dbh.DBClose();
+		}
+		return result;	
+	}
+	public static int logoutPosition(String gener_id) throws SQLException{
+		int result = 0;
+		if (gener_id == null){
+			result = -1;
+		}
+		else{
+			String sql = "update position set "
+					+ "pos_iscurrent = 0 "
+					+ "where gener_id = " + gener_id + " and pos_iscurrent = 1";
+			DBHelper dbh = new DBHelper();
+			result = dbh.updateDatabase(sql);
+			dbh.DBClose();
+		}
+		return result;	
+	}
+}
---
+++ GPSRouting/src/com/bean/Route.java
@@ -0,0 +1,138 @@
+package com.bean;
+
+import java.sql.ResultSet;
+import java.sql.SQLClientInfoException;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import com.util.DBHelper;
+
+
+public class Route {
+	private int gener_id;
+	//private String gener;
+	private float longitude;
+	private float latitude;
+	private int islogin;
+	private String time;
+	private int rtr_id;
+	public Route(int gener_id, float longitude, float latitude, int islogin,
+			String time, int rtr_id) {
+		super();
+		this.gener_id = gener_id;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.islogin = islogin;
+		this.time = time;
+		this.rtr_id = rtr_id;
+	}
+	public int getGener_id() {
+		return gener_id;
+	}
+	public void setGener_id(int gener_id) {
+		this.gener_id = gener_id;
+	}
+	public float getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+	public float getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+	public int getIslogin() {
+		return islogin;
+	}
+	public void setIslogin(int islogin) {
+		this.islogin = islogin;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public int getRtr_id() {
+		return rtr_id;
+	}
+	public void setRtr_id(int rtr_id) {
+		this.rtr_id = rtr_id;
+	}
+	
+	public static JSONArray GetAllRoute(String gener_id, Timestamp start, Timestamp end) throws SQLException{
+		JSONArray positions= new JSONArray();
+		String sql = "select people_name, route_longitude, route_latitude, route_time, route_islogin "
+				+ "from route inner join people "
+				+ "on people_id = route.gener_id "
+				+ "where route.gener_id = " + gener_id +" and "
+				+ "route_time < " + end + " and route_time > " + start + " order by route_time";
+		DBHelper dbh = new DBHelper();
+		ResultSet rs = dbh.getResultSet(sql);
+		while(rs.next()){
+			JSONObject pos = new JSONObject();
+			pos.put("gener", rs.getString(1));
+			pos.put("longitude", rs.getString(2));
+			pos.put("latitude", rs.getString(3));
+			pos.put("time", rs.getString(4));
+			pos.put("islogin", rs.getString(5));
+			positions.add(pos);
+		}
+		dbh.DBClose(rs);
+		return positions;
+	}
+//	public static JSONArray GetAllRoute(String rtr_id) throws SQLException{
+//		JSONArray positions= new JSONArray();
+//		//Map<String, String> position = new HashMap<String, String>();
+//		String sql = "select people_name, route_longitude, route_latitude, route_time, rtr_name"
+//				+ "from (route inner join people "
+//				+ "on people_id = route.gener_id) "
+//				+ "inner join regiontoregion "
+//				+ "on regiontoregion.rtr_id = route.rtr_id"
+//				+ "where route.rtr_id = " + rtr_id ;
+//		DBHelper dbh = new DBHelper();
+//		ResultSet rs = dbh.getResultSet(sql);
+//		while(rs.next()){
+//			JSONObject pos = new JSONObject();
+//			pos.put("gener", rs.getString(1));
+//			pos.put("longitude", rs.getString(2));
+//			pos.put("latitude", rs.getString(3));
+//			pos.put("time", rs.getString(4));
+//			pos.put("rtr_name", rs.getString(5));
+//			positions.add(pos);
+//		}
+//		dbh.DBClose(rs);
+//		return positions;
+//	}
+	public static int addOneRoute(Route rt) throws SQLException{
+		int result = 0;
+		if (rt == null){
+			result = -1;
+		}
+		else{
+			String sql = "insert into route"
+					+ "(gener_id, route_longitude, route_latitude, route_islogin, route_time, rtr_id)"
+					+ " values('"+rt.getGener_id()+ "','" 
+					+ rt.getLongitude() + "','" 
+					+ rt.getLatitude() + "','" 
+					+ rt.getIslogin() + "','"
+					+ rt.getTime()  + "','" 
+					+ rt.getRtr_id()  + "')"; 
+			DBHelper dbh = new DBHelper();
+			result = dbh.updateDatabase(sql);
+			dbh.DBClose();
+		}
+		return result;	
+	}
+	
+}

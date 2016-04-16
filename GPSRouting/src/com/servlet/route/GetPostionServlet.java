268624c3250d4d8f806ede4ee62dd4ebb0f07196
lixin1994 <lixin_cug@foxmail.com>
2016-04-16 21:54:34 +0800

    完善Servlet

--- GPSRouting/src/com/servlet/route/GetPostionServlet.java
+++ GPSRouting/src/com/servlet/route/GetPostionServlet.java
@@ -1,11 +1,17 @@
 package com.servlet.route;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.bean.Position;
+import com.util.OutputHelper;
 
 /**
  * Servlet implementation class GetPostionServlet
@@ -34,6 +40,47 @@ public class GetPostionServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String index = request.getParameter("index");
+		if(index.equals("all")){
+			try {
+				OutputHelper.StringOutPut(Position.GetAllPosition().toString(), response);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else if (index.equals("gener")){
+			String gener_id = request.getParameter("gener_id");
+			if (gener_id == null) {				
+				OutputHelper.StringOutPut("error_gener", response);
+				return;
+			}
+			
+			try {
+				
+				OutputHelper.StringOutPut(Position.GetAllPosition(gener_id).toString(), response);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else if (index.equals("history")) {
+			String gener_id = request.getParameter("gener_id");
+			String start = request.getParameter("start");
+			String end = request.getParameter("end");
+			if (gener_id == null||start==null||end ==null) {				
+				OutputHelper.StringOutPut("error_history", response);
+				return;
+			}
+			try {
+				OutputHelper.StringOutPut(Position.GetAllPosition(gener_id,Timestamp.valueOf(start),Timestamp.valueOf(end)).toString(), response);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else {
+			OutputHelper.StringOutPut("error_index", response);
+		}
 	}
 
 }
--- GPSRouting/src/com/servlet/route/GetRouteServlet.java
+++ GPSRouting/src/com/servlet/route/GetRouteServlet.java
@@ -1,11 +1,18 @@
 package com.servlet.route;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.bean.Position;
+import com.bean.Route;
+import com.util.OutputHelper;
 
 /**
  * Servlet implementation class GetRouteServlet
@@ -27,6 +34,19 @@ public class GetRouteServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String gener_id = request.getParameter("gener_id");
+		String start = request.getParameter("start");
+		String end = request.getParameter("end");
+		if (gener_id == null||start==null||end ==null) {				
+			OutputHelper.StringOutPut("error_index", response);
+			return;
+		}
+		try {
+			OutputHelper.StringOutPut(Route.GetAllRoute(gener_id,Timestamp.valueOf(start),Timestamp.valueOf(end)).toString(), response);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
